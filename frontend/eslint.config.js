79dc0af8fde09b69d991d042c5b85ea7ee922493
KrishTday <krishgammer@gmail.com>
2026-09-17 15:38:22 -0400

    fix: update ESLint config for TypeScript and CommonJS compatibility

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,23 +1,39 @@
 import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import nextPlugin from '@next/eslint-plugin-next';
 
 export default [
   {
-    ignores: ['.next', 'node_modules', 'out'],
+    ignores: ['.next', 'node_modules', 'out', '*.config.js'],
   },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     plugins: {
       '@next/next': nextPlugin,
     },
     rules: {
-      ...js.configs.recommended.rules,
       ...nextPlugin.configs.recommended.rules,
       ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
+  {
+    files: ['**/*.config.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+    },
+    rules: {
+      'no-undef': 'off',
     },
   },
 ];
