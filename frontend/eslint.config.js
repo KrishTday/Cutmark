e43ad93ee7348327b5c95fd1545bb66ebb431801
KrishTday <krishgammer@gmail.com>
2026-09-17 15:53:17 -0400

    fix: simplify ESLint config for Next.js 16 compatibility

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,38 +4,24 @@ import nextPlugin from '@next/eslint-plugin-next';
 
 export default [
   {
-    ignores: ['.next', 'node_modules', 'out'],
+    ignores: ['.next', 'node_modules', 'out', '.turbo'],
   },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
+    ...js.configs.recommended,
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    ...tseslint.configs.recommended,
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
     plugins: {
       '@next/next': nextPlugin,
     },
     rules: {
       ...nextPlugin.configs.recommended.rules,
       ...nextPlugin.configs['core-web-vitals'].rules,
-    },
-  },
-  {
-    files: ['*.config.js'],
-    languageOptions: {
-      globals: {
-        module: 'readonly',
-      },
-    },
-    rules: {
-      'no-undef': 'off',
     },
   },
 ];
